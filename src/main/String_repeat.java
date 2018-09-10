2c0127be761691696daba03596603cffc34bd041
CGI-Praneeth <praneethvikram635@gmail.com>
2018-09-10 16:18:43 +0530

    new update

---
+++ src/main/String_repeat.java
@@ -0,0 +1,35 @@
+package main;
+
+import java.util.Scanner;
+import java.lang.*;
+public class String_repeat {
+
+    public static void main(){
+
+        Scanner reader = new Scanner(System.in);
+        String str = reader.nextLine();
+        int number ;
+        number=reader.nextInt();
+        int length=str.length();
+        int extra_length=length-number;
+        String ans="";
+        String result="";
+        int i;
+        for(i=length-1;i>=extra_length;i--){
+
+            ans=ans+str.charAt(i);
+        }
+
+        for(i=0;i<number;i++) {
+
+            str=str + ans;
+
+
+        }
+
+         System.out.println(str);
+
+
+
+    }
+}
